1ae24cc2d38085c83b64c443c1a0004f1f7e6850
sanj64 <mailatsanjukta@gmail.com>
2025-07-20 18:24:18 +0530

    Creating Thermal module folder and correcting minor errors

---
+++ Thermal/test.py
@@ -0,0 +1,10 @@
+# example code for Thermal module
+
+def thermal_placeholder():
+    """
+    This is a placeholder function for thermal module.
+    Replace or edit file(s) for future versions.
+    """
+    print("thermal logic module under development")
+
+thermal_placeholder()
